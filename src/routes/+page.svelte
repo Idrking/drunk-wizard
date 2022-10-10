625c4ad7328942d66c9c469abc177c6bb42afe71
Idrking <idrkings@gmail.com>
2022-10-10 14:50:34 -0700

    Hooked up to external API, add button to change word

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,28 @@
 	getWords().then((res) => {
 		word = res.data;
 	});
+
+	async function newWord() {
+		console.log('running');
+		const newWord = await getWords();
+		word = newWord.data;
+	}
 </script>
 
 <h1>{word}</h1>
+<button on:click={newWord}>New Word!</button>
 
+<!-- 
+<script>
+	let word = 'duck';
+	async function getWords() {
+		const response = await fetch('http://localhost:5173/api/generate', { method: 'GET' });
+		return await response.json();
+	}
+
+	getWords().then((res) => {
+		word = res.data;
+	}); -->
 <style>
 	h1 {
 		font-size: 4rem;
--- src/routes/api/generate/+server.js
+++ src/routes/api/generate/+server.js
@@ -1,6 +1,14 @@
 import { error, json } from '@sveltejs/kit';
 import { NINJA_API_KEY } from '$env/static/private';
 
-export function GET() {
-	return json({ data: 'Tigers' });
+export async function GET() {
+	let res = await fetch('https://api.api-ninjas.com/v1/randomword', {
+		method: 'GET',
+		headers: { 'X-API-KEY': NINJA_API_KEY },
+		contentType: 'application/json'
+	});
+
+	res = await res.json();
+
+	return json({ data: res.word });
 }
