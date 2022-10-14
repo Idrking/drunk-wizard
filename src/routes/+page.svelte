6d12ea5e2837f3c1fa75957919f7cf4211037cc1
Idrking <idrkings@gmail.com>
2022-10-14 11:18:46 -0700

    Add global styles/tokens, and removed test logic from main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,11 @@
 <script>
-	let word = 'duck';
-	let name = String.fromCharCode(Math.floor(65 + Math.random() * (90 - 65 + 1)));
+	let word = '';
 	async function getWords() {
 		const response = await fetch('http://localhost:5173/api/generate', { method: 'GET' });
-		return await response.json();
+		const data = await response.json();
+		word = data.spell;
+		return;
 	}
-
-	getWords().then((res) => {
-		word = res.spell;
-		name = res.name;
-	});
-
 	async function newWord() {
 		const newWord = await getWords();
 		word = newWord.data;
@@ -18,12 +13,11 @@
 </script>
 
 <h1>{word}</h1>
-<h1>{name}</h1>
 <button on:click={newWord}>New Word!</button>
 
 <style>
 	h1 {
 		font-size: 4rem;
-		color: deeppink;
+		color: var(--orange-main);
 	}
 </style>
